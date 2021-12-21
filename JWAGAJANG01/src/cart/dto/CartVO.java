df5f73dae00e926b6938157e9a0db37253c725a7
SJ25O <tarae526@gmail.com>
2021-12-21 17:59:43 +0900

    1221 민선지 커밋
    ProductVO수정과 DAO 일부 추가
    Cart 관련 코드 다량 추가
    캬 오늘 역대급으로 열심히한듯?

--- JWAGAJANG01/src/cart/dto/CartVO.java
+++ JWAGAJANG01/src/cart/dto/CartVO.java
@@ -3,16 +3,20 @@ package cart.dto;
 public class CartVO {
 	private String md_code;
 	private String md_name;
+	private String img_main;
 	private int md_price;
+	private int md_dc;
 	private int md_count;
 	
 	public CartVO () {}
 	
-	public CartVO(String md_code, String md_name, int md_price, int md_count) {
+	public CartVO(String md_code, String md_name, String img_main, int md_price, int md_dc, int md_count) {
 		super();
 		this.md_code = md_code;
 		this.md_name = md_name;
+		this.img_main = img_main;
 		this.md_price = md_price;
+		this.md_dc = md_dc;
 		this.md_count = md_count;
 	}
 
@@ -32,12 +36,28 @@ public class CartVO {
 		this.md_name = md_name;
 	}
 
+	public String getImg_main() {
+		return img_main;
+	}
+
+	public void setImg_main(String img_main) {
+		this.img_main = img_main;
+	}
+
 	public int getMd_price() {
 		return md_price;
 	}
 
 	public void setMd_price(int md_price) {
 		this.md_price = md_price;
+	}
+
+	public int getMd_dc() {
+		return md_dc;
+	}
+
+	public void setMd_dc(int md_dc) {
+		this.md_dc = md_dc;
 	}
 
 	public int getMd_count() {
