953459a83a1451eae4325aa45997873c8df1f232
Harry <tlqfk__@naver.com>
2021-12-22 18:34:45 +0900

    dao빼오기

--- JWAGAJANG01/src/member/dao/FavDao.java
+++ JWAGAJANG01/src/member/dao/FavDao.java
@@ -30,24 +30,24 @@ public class FavDao {
 					+ "(\r\n"
 					+ "  SELECT \r\n"
 					+ "      @rownum:=@rownum+1  rnum, \r\n"
-					+ "      jwagajang.fav.* \r\n"
+					+ "      jwagajang.table_fav.* \r\n"
 					+ "  FROM \r\n"
-					+ "      jwagajang.fav, \r\n"
+					+ "      jwagajang.table_fav, \r\n"
 					+ "      (SELECT @ROWNUM := 0) R\r\n"
 					+ "  WHERE \r\n"
 					+ "      1=1\r\n"
 					+ ") list\r\n"
-					+ "WHERE rnum >= ? AND rnum <= ? AND mdid = ?;";
+					+ "WHERE rnum >= ? AND rnum <= ? AND user_id = ?;";
 			pstmt = conn.prepareStatement(query);
 			pstmt.setInt(1, 1+(page_-1)*5);// 등차수열 1 + (page_ - 1) * 5
 			pstmt.setInt(2, page_*5);
 			pstmt.setString(3, id);
 			rs = pstmt.executeQuery();
 			while(rs.next()) {
-				String mdpic = rs.getString("mdpic");
-				String mdname = rs.getString("mdname");
-				Date mdorderdate = rs.getDate("mdorderdate");
-				String mdprice = rs.getString("mdprice");
+				String mdpic = rs.getString("order_pic");
+				String mdname = rs.getString("order_name");
+				Date mdorderdate = rs.getDate("order_regdate");
+				String mdprice = rs.getString("order_price");
 				
 				order = new Order(
 						mdpic,mdname,mdorderdate,mdprice
@@ -78,14 +78,14 @@ public class FavDao {
 					+ "(\r\n"
 					+ "  SELECT \r\n"
 					+ "      @rownum:=@rownum+1  rnum, \r\n"
-					+ "      jwagajang.fav.* \r\n"
+					+ "      jwagajang.table_fav.* \r\n"
 					+ "  FROM \r\n"
-					+ "      jwagajang.fav, \r\n"
+					+ "      jwagajang.table_fav, \r\n"
 					+ "      (SELECT @ROWNUM := 0) R\r\n"
 					+ "  WHERE \r\n"
 					+ "      1=1\r\n"
 					+ ") list\r\n"
-					+ "WHERE mdid = ?;";
+					+ "WHERE user_id = ?;";
 			
 			pstmt = conn.prepareStatement(query);
 			pstmt.setString(1, id);
@@ -109,7 +109,7 @@ public class FavDao {
 	public void delete(Connection conn,String id,String mdname) {
 		PreparedStatement pstmt = null;
 		try {
-			String query = "DELETE FROM `jwagajang`.`fav` WHERE (`mdname` = ?  and `mdid` = ?);";
+			String query = "DELETE FROM `jwagajang`.`table_fav` WHERE (`order_name` = ?  and `user_id` = ?);";
 			
 			pstmt = conn.prepareStatement(query);
 			pstmt.setString(1, mdname);
